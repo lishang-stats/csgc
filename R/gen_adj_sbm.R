f3863354f1c9d34d986a3298d28ea06b6441a512
Shang Li <lishang@u.nus.edu>
2023-01-17 12:23:18 +0800

    add function: gen_adj_sbm

---
+++ R/gen_adj_sbm.R
@@ -0,0 +1,33 @@
+#' Generate adjacency matrix from Stochastic Block Model
+#'
+#' @param K connection probability matrix
+#' @param z community labels
+#'
+#' @return adjacency matrix (A) and probability matrix (P)
+#' @export
+#'
+#' @examples
+#' library(csgc)
+#' prob = c(0.8, 0.1)
+#' K = matrix(prob[2],k,k)
+#' diag(K) = prob[1]
+#' z = rep(1:k,each=n/k)
+#' gen_adj_sbm(K,z)
+
+gen_adj_sbm <- function(K,z){
+  # INPUT
+  # K = connection probability matrix
+  # z = labels
+  #
+  # OUTPUT
+  # A:  adjacency matrix
+  # P:  probability matrix
+  k = dim(K)[1]
+  n = length(z)
+  P = K[z,z]
+  diag(P) = 0
+  A = matrix(0,n,n)
+  A[upper.tri(A)] = rbinom(n*(n-1)/2,1,P[upper.tri(P)])
+  A = A+t(A)
+  return(list(A=A,P=P))
+}
