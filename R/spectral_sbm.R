4f0518d50b15450f4265db0b3c8509ce373483d4
Shang Li <lishang@u.nus.edu>
2023-01-18 12:56:33 +0800

    add function: spectral_sbm

---
+++ R/spectral_sbm.R
@@ -0,0 +1,31 @@
+#' Spectral clustering for Stochastic Block Model
+#'
+#' @param A adjacency matrix
+#' @param k number of communities
+#'
+#' @return estimated labels
+#' @export
+#' @importFrom irlba irlba
+#' @importFrom stats kmeans
+#'
+#' @examples
+#' library(csgc)
+#' k = 4
+#' n = 200
+#' prob = c(0.8, 0.1)
+#' K = matrix(prob[2],k,k)
+#' diag(K) = prob[1]
+#' z = rep(1:k,each=n/k)
+#' A = gen_adj_sbm(K,z)$A
+#' spectral_sbm(A,k)
+spectral_sbm <- function(A,k){
+  # required package: irlba, stats
+  # INPUT
+  # A = adjacency matrix
+  # k = number of communities
+  #
+  # OUTPUT
+  # labels: estimated labels
+  labels = kmeans(irlba(A,k)$v,k)$cluster
+  return(labels)
+}
